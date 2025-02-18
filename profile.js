ec5053511741782fc0328840bda9285fc260c8f0
Rafael23082 <144778682+Rafael23082@users.noreply.github.com>
2025-02-18 17:53:04 +0700

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,11 +1,5 @@
-let totalRating = 0;
-let totalSubmissions = 0;
-
 function addRating(rating){
-    totalRating += rating;
-    totalSubmissions ++;
-    let averageRating = (totalRating / totalSubmissions).toFixed(1);
-    document.getElementById("rating-value").innerText = `My Average Rating: ${averageRating}.`
+    document.getElementById("rating-value").innerText = `My Average Rating: ${rating}.`
 }
 
 const menuButton = document.getElementById("menu-btn");
